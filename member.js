b72efbee8a24985a9eee5981843041a47f6d3553
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2023-10-29 18:23:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,65 @@
+function skillsMember() {
+    var vm = this;
+    vm.skills = [
+        {
+            name: 'HTML5',
+            icon: 'html5',
+            url: 'https://en.wikipedia.org/wiki/HTML5',
+            color: '#E34F26'
+        },
+        {
+            name: 'CSS3',
+            icon: 'css3',
+            url: 'https://en.wikipedia.org/wiki/Cascading_Style_Sheets',
+            color: '#1572B6'
+        },
+        {
+            name: 'JavaScript',
+            icon: 'javascript',
+            url: 'https://en.wikipedia.org/wiki/JavaScript',
+            color: '#F7DF1E'
+        },
+        {
+            name: 'AngularJS',
+            icon: 'angularjs',
+            url: 'https://angularjs.org/',
+            color: '#E23237'
+        },
+        {
+            name: 'NodeJS',
+            icon: 'nodejs',
+            url: 'https://nodejs.org/',
+            color: '#215732'
+        },
+        {
+            name: 'MongoDB',
+            icon: 'mongodb',
+            url: 'https://www.mongodb.org/',
+            color: '#3FA037'
+        },
+        {
+            name: 'PHP',
+            icon: 'php',
+            url: 'https://secure.php.net/',
+            color: '#777BB3'
+        },
+        {
+            name: 'MySQL',
+            icon: 'mysql',
+            url: 'https://www.mysql.com/',
+            color: '#4479A1'
+        },
+        {
+            name: 'WordPress',
+            icon: 'wordpress',
+            url: 'https://wordpress.org/',
+            color: '#21759B'
+        },
+        {
+            name: 'Git',
+            icon: 'git',
+            url: 'https://git-scm.com/',
+            color: '#F05032'
+        }
+    ];
+}
